7c6156d3ab72f55595a3f8ae5ed02ff7c6910f93
Nick Warms <nwarms96@gmail.com>
2024-06-17 17:42:53 +0100

    Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,24 @@
-import Button from "@/Components/Button";
 import UserComponent from "@/Components/UserComponent";
 import TeamComponent from "@/Components/TeamComponent";
-import Grid from "@/Components/Grid";
-import { writeUserMatches, assignTeamsToUsers } from "@/Helpers/SweepstakeHelpers";
 import prisma from "@/lib/prisma";
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
-  const teams = await prisma.team.findMany();
-  const matches = await prisma.teamAssigned.findMany();
+  const matches = await prisma.teamAssigned.findMany({include:{team:true, user:true}});
   return (
     <main className="flex flex-col">
       <div className='flex flex-row gap-5'>
-        <div className='w-30% flex flex-col'>
-          <div>
-            <h2>Teams</h2>
-            <ul>
-              <Grid>
-                {teams.map(team => {
-                  return (
-                    <li key={team.name}><TeamComponent team={team} /></li>
-                  )
-                })}
-              </Grid>
-            </ul>
-          </div>
-          <div>
-            <h2>
-              Users
-            </h2>
-            <Grid>
-              {users.map(item => {
-                return <UserComponent key={item.id} user={item} />
-              })}
-            </Grid>
-          </div>
-        </div>
         <div>
           <h2>
             Drawn list
           </h2>
-          <Grid>
+          <div className="grid grid-cols-4 md:grid-cols-8">
             {matches.map(item => {
-              return <div key={item.id} className=" hover:bg-slate-600 p-2">
-                <UserComponent user={users.find(user => user.id === item.user_id)} />
-                <TeamComponent team={teams.find(team => team.id === item.team_id)} />
+              return <div key={item.id} className="p-2">
+                <UserComponent user={item.user} />
+                <TeamComponent team={item.team} />
               </div>
             })}
-          </Grid>
+          </div>
         </div>
       </div>
     </main>
