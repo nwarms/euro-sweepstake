42766702b076309c00d8818a87e6fb2563b3688d
Nick Warms <nwarms96@gmail.com>
2024-06-17 17:44:56 +0100

    Add extended types

--- Types/prismaExtendedTypes.ts
+++ Types/prismaExtendedTypes.ts
@@ -1,4 +1,26 @@
-import {Prisma} from '@prisma/client'
-export type MatchWithTeams = Prisma.MatchGetPayload<{include:{homeTeam:true, awayTeam:true}}>;
-export type MatchWithStage = Prisma.MatchGetPayload<{include:{stage:true}}>;
+import { Prisma } from '@prisma/client'
+export type MatchWithTeams = Prisma.MatchGetPayload<{ include: { homeTeam: true, awayTeam: true } }>;
+export type MatchWithStage = Prisma.MatchGetPayload<{ include: { stage: true } }>;
 export type MatchFull = MatchWithStage & MatchWithTeams;
+
+export type TeamWithMatches = Prisma.TeamGetPayload<{ include: { homeMatches: true, awayMatches: true } }>;
+export type TeamWithFullMatches = Prisma.TeamGetPayload<{
+    include: {
+        homeMatches: {
+            include: {
+                homeTeam: true,
+                awayTeam: true,
+                stage: true
+            }
+        },
+        awayMatches: {
+            include: {
+                homeTeam: true,
+                awayTeam: true,
+                stage: true
+            }
+        }
+    }
+}>;
+
+export type TeamAssignedFull = Prisma.TeamAssignedGetPayload<{include:{user:true, team:true}}>;
